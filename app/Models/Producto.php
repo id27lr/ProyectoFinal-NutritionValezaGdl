4e7f44a33bdc41a78081f05a7a7aa1f0febf88c6
Roberto Lopez <roberloplo23@gmail.com>
2024-11-16 18:52:27 -0600

    modelo controlador y request de producto

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
+    use HasFactory;
+    
+    protected $table = 'producto';
+    protected $primarKey = 'id';
+
+    public $timestamps = false;
+
+    protected $fillable = ['id_categoria','codigo','stock', 'descripcion', 'imagen','estado'];
     //
 }
