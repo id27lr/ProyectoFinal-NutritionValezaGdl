ed923517ef9c70e902fe04df20eee266cb932ae6
Ian Lopez <iand.lopezrios@gmail.com>
2024-11-22 17:33:29 -0600

    Funciones JS para agregar ingresos

--- resources/views/compras/ingreso/create.blade.php
+++ resources/views/compras/ingreso/create.blade.php
@@ -1,4 +1,5 @@
 @extends('adminlte::page')
+@extends('layouts.app')
 
 @section('content_header')
 <div class="card-header">
@@ -24,21 +25,21 @@
                 </div>
                 <div class="col-3">
                     <div class="form-group">
-                        <label for="tipo_documento">Tipo Documento</label>
-                        <select name="tipo_documento" class="form-control @error('tipo_documento') is-invalid @enderror" id="tipo_documento">
-                            <option value="RFC" {{ old('tipo_documento') == 'RFC' ? 'selected' : '' }}>RFC</option>
-                            <option value="INE" {{ old('tipo_documento') == 'INE' ? 'selected' : '' }}>INE</option>
+                        <label for="comprobante">Tipo Documento</label>
+                        <select name="comprobante" class="form-control @error('comprobante') is-invalid @enderror" id="comprobante">
+                            <option value="RFC" {{ old('comprobante') == 'RFC' ? 'selected' : '' }}>RFC</option>
+                            <option value="INE" {{ old('comprobante') == 'INE' ? 'selected' : '' }}>INE</option>
                         </select>
-                        @error('tipo_documento')
+                        @error('comprobante')
                         <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
                 </div>
                 <div class="col-3">
                     <div class="form-group">
-                        <label for="num_documento">Número Documento</label>
-                        <input type="text" class="form-control @error('num_documento') is-invalid @enderror" name="num_documento" id="num_documento" placeholder="Ingresa el número del documento" value="{{ old('num_documento') }}">
-                        @error('num_documento')
+                        <label for="num_comprobante">Número Documento</label>
+                        <input type="text" class="form-control @error('num_comprobante') is-invalid @enderror" name="num_comprobante" id="num_comprobante" placeholder="Ingresa el número del documento" value="{{ old('num_comprobante') }}">
+                        @error('num_comprobante')
                         <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
@@ -49,7 +50,7 @@
                 <div class="col-4">
                     <div class="form-group">
                         <label for="proveedor">Productos</label>
-                        <select name="id_proveedor" class="form-control @error('id_productos') is-invalid @enderror" id="id_productos">
+                        <select name="id_proveedor" class="form-control @error('id_producto') is-invalid @enderror" id="id_producto">
                             @foreach($productos as $producto)
                             <option value="{{$producto->id}}">{{$producto->Producto}}</option>
                             @endforeach
@@ -59,7 +60,7 @@
                 <div class="col-2">
                     <div class="form-group">
                         <label for="cantidad">Cantidad</label>
-                        <input type="number" class="form-control" name="pcantidad" id="pcantidad" placeholder="Cantidad">
+                        <input type="number" class="form-control" name="cantidad" id="cantidad" placeholder="Cantidad">
                     </div>
                 </div>
                 <div class="col-2">
@@ -76,14 +77,14 @@
                 </div>
                 <div class="col-2">
                     <div class="form-group">
-                        <label for="proveedor">Acción</label>
-                        <button type="button" id="btn_add" class="btn btn-success me-1 mb-1"></button>
+                        <label for="accion">Acción</label>
+                        <button type="button" id="btn_add" class="btn btn-block btn-outline-success">Agregar</button>
                     </div>
                 </div> 
 
                 <div class="col-12">
                     <div class="table-responsive">
-                        <table class="table table-hover mb-0">
+                        <table  id="detalles" class="table table-bordered mb-0">
                             <thead>
                                 <tr>
                                     <th>Opciones</th>
@@ -92,12 +93,10 @@
                                     <th>Precio Compra</th>
                                     <th>Precio Venta</th>
                                     <th>Subotal</th>
-                                    <th>Estatus</th>
                                 </tr>
                             </thead>
                             <tfoot>
                                 <th>TOTAL</th>
-                                <th></th>
                                 <th></th>
                                 <th></th>
                                 <th></th>
@@ -112,6 +111,7 @@
                 </div>
 
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
                 <div class="card-footer">
                     <button type="submit" class="btn btn-success me-1 mb-1">Guardar</button>
                     <button type="reset" class="btn btn-danger me-1 mb-1">Cancelar</button>
@@ -120,4 +120,84 @@
 
     </form>
 </div>
+
+@push('scipts')
+    <script>
+        
+        $(document).ready(function(){
+            $("#btn_add").click(function(){
+                agregar();
+            });
+        });
+
+        var cont = 0;
+        total = 0;
+        subtotal = [];
+
+
+        $("#guardar").hide();
+        $("#id_producto").change(mostrarValores);
+
+
+        function mostrarValores(){
+            datosProducto = document.getElementById('id_producto').value.split('_');
+            $("#cantidad").val(datosProducto[1]);
+        }
+        
+        function agregar(){
+        
+            datosProducto = document.getElementById('id_producto').value.split('_');
+            id_producto = datosProducto[0];
+            producto = $("#id_producto option:selected").text();
+            
+            cantidad = $("#cantidad").val();
+            precio_compra = $("#precio_compra").val();
+            precio_venta = $("#precio_venta").val();
+            
+            if (id_producto != "" && cantidad != "" && cantidad > 0 && precio_compra != "" && precio_venta != ""){
+            
+                subtotal[cont] = precio_compra * cantidad;
+                total = total + subtotal[cont];
+            
+                var fila = '<tr class="selected" id="fila' + cont + 
+                            '"> <td><button type="button" class="btn btn-warning" onclick="eliminar(' + cont + 
+                                ')";>X</button></td><td><input type="hidden" name="id_producto[]" value="' + id_producto + '">' + producto +
+                                    '</td><td><input type="number" name="cantidad[]" value="' + cantidad +
+                                        '"></td> <td><input type="number" name="precio_compra[]" value="' + precio_compra +
+                                            '"></td> <td> <input type="number" name="precio_venta[]" value="' + precio_venta +
+                                                '"</td><td>' + subtotal[cont] + '</td></tr>';
+                cont++;
+                limpiar();
+                $("#total").html("$:" + total);
+                evaluar();
+                $("#detalles").append(fila);
+        
+            }else{
+                alert("Error al ingresar los datos del artículo");
+            }
+
+        }
+
+        function limpiar(){
+            $("#cantidad").val("");
+            $("#precio_compra").val("");
+            $("#precio_venta").val("");
+        }
+        
+        function evaluar(){
+            if (total > 0) {
+                $("#guardar").show();
+            } else {
+                $("#guardar").show();
+            }
+        }
+        
+        function eliminar(index){
+            total = total - subtotal[index];
+            $("#total").html("$: " + total);
+            $("#fila" + index).remove();
+        }
+    </script>
+@endpush
+
 @endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    @stack('styles') 
+</head>
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <!-- Contenido principal -->
+        
+    </div>
+
+    <!-- Scripts principales de AdminLTE -->
+    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+
+    
+    @stack('scripts')
+
+</body>
+</html>
