0169686de51b7a6dee90cfa9171aa1bce8a7adbb
Ian Lopez <iand.lopezrios@gmail.com>
2024-11-22 20:44:46 -0600

    Modelos y Request de Venta y DetalleVenta

--- app/Models/DetalleVenta.php
+++ app/Models/DetalleVenta.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DetalleVenta extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $table = 'detalle_ventas';
+    protected $primarKey = 'id';
+
+    public $timestamps = false;
+
+    protected $fillable = ['id_venta', 'id_producto', 'cantidad', 'precio_venta', 'descuento'];
 }
--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Venta extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $table = 'ventas';
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['id_cliente','tipo_comprobante','num_comprobante', 'fecha_hora', 'impuesto', 'total_venta', "estatus"];
 }
