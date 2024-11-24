5ead8644ceb0f0ebd557923696340678735862ce
Ian Lopez <iand.lopezrios@gmail.com>
2024-11-24 00:55:30 -0600

    Proteccion de rutas, acceso solo a usuarios autenticados

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,32 @@ use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\VentaController;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::resource('almacen/categoria', CategoriaController::class);
-Route::resource('almacen/producto', ProductoController::class);
-Route::resource('ventas/clientes', ClienteController::class);
-Route::resource('compras/proveedor', ProveedorController::class);
-Route::resource('compras/ingreso', IngresoController::class);
-Route::resource('ventas/venta', VentaController::class);
+Route::middleware('auth')->group(function () {
+    
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::group(['middleware' => ['role:admin']], function () {
+    Route::resource('almacen/categoria', CategoriaController::class);
+    Route::resource('almacen/producto', ProductoController::class);
+    Route::resource('ventas/clientes', ClienteController::class);
+    Route::resource('compras/proveedor', ProveedorController::class);
+    Route::resource('compras/ingreso', IngresoController::class);
+    Route::resource('ventas/venta', VentaController::class);
+
+    Route::group(['middleware' => ['role:admin']], function () {
+        
+        Route::resource('seguridad/usuarios', UsuarioController::class);
+        Route::resource('seguridad/roles', UserRoleController::class);
+        
+        Route::get('seguridad/usuarios/{user}/roles', [UserRoleController::class, 'edit'])->name('seguridad.roles.edit');
+        Route::put('seguridad/usuarios/{user}/roles', [UserRoleController::class, 'update'])->name('seguridad.roles.update');
+    });
+
     
-    Route::resource('seguridad/usuarios', UsuarioController::class);
-    Route::resource('seguridad/roles', UserRoleController::class);
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     
-    Route::get('seguridad/usuarios/{user}/roles', [UserRoleController::class, 'edit'])->name('seguridad.roles.edit');
-    Route::put('seguridad/usuarios/{user}/roles', [UserRoleController::class, 'update'])->name('seguridad.roles.update');
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
